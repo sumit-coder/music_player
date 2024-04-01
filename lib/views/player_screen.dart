b959652923ab87cb17f6200947a760a4d07d9647
sumit-coder <sumitbu24@gmail.com>
2024-04-01 20:05:18 +0530

    initial commit

---
+++ lib/views/player_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PlayerScreen extends StatelessWidget {
+  const PlayerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
