8a4e45334045e56fbe48da5d7e3843f44665d162
sumit-coder <sumitbu24@gmail.com>
2024-04-02 16:44:47 +0530

    added playback timeline

--- lib/views/player_screen.dart
+++ lib/views/player_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:music_player/views/widgets/player_widgets/play_button.dart';
 import 'package:music_player/views/widgets/player_widgets/repeat_button.dart';
 import 'package:music_player/views/widgets/player_widgets/shuffle_button.dart';
@@ -8,29 +9,72 @@ class PlayerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.grey.shade900,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
+          // Music Cover Album
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.network(
+              "https://i.ytimg.com/vi/725WlG1idPc/maxresdefault.jpg",
+              width: size.width * 0.80,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+          ),
+          // Playback Timeline
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 14),
+            child: Column(
+              children: [
+                SliderTheme(
+                  data: SliderThemeData(
+                    overlayShape: SliderComponentShape.noOverlay,
+                    thumbShape: const RoundSliderThumbShape(enabledThumbRadius: 6),
+                    thumbColor: Colors.grey,
+                    activeTrackColor: Colors.grey.shade600,
+                    inactiveTrackColor: Colors.grey.shade800,
+                    trackHeight: 2,
+                  ),
+                  child: Slider(
+                    value: 0.6,
+                    onChanged: (value) {
+                      print(value);
+                    },
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('5:00', style: TextStyle(color: Colors.grey)),
+                      Text('0:00', style: TextStyle(color: Colors.grey)),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
           // Playback Buttons
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              // IconButton(onPressed: () {}, icon: Icon(Icons.shuffle_rounded, size: 24)),
               ShuffleButton(
                 isShuffleOn: false,
                 onChange: (newValue) {},
               ),
-              IconButton(onPressed: () {}, icon: Icon(Icons.skip_previous_rounded, size: 34)),
-              // IconButton(onPressed: () {}, icon: Icon(Icons.play_circle_fill_rounded, size: 44)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.skip_previous_rounded, size: 34)),
               PlayButton(
                 isPlaying: true,
                 onChange: (bool newValue) {
                   print(newValue);
                 },
               ),
-              IconButton(onPressed: () {}, icon: Icon(Icons.skip_next_rounded, size: 34)),
-              // IconButton(onPressed: () {}, icon: Icon(Icons.repeat_rounded, size: 24)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.skip_next_rounded, size: 34)),
               RepeatButton(
                 isRepeatOn: false,
                 onChange: (newValue) {},
