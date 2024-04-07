135e3de0fa77c994e131cf6586e53c3a22056b12
sumit-coder <sumitbu24@gmail.com>
2024-04-07 23:10:26 +0530

    music can be played now more work need

--- lib/views/player_screen.dart
+++ lib/views/player_screen.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:music_player/views/widgets/player_widgets/play_button.dart';
 import 'package:music_player/views/widgets/player_widgets/repeat_button.dart';
 import 'package:music_player/views/widgets/player_widgets/shuffle_button.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 
-class PlayerScreen extends StatelessWidget {
+class PlayerScreen extends StatefulWidget {
   final SongModel songInfo;
 
   const PlayerScreen({super.key, required this.songInfo});
+
+  @override
+  State<PlayerScreen> createState() => _PlayerScreenState();
+}
+
+class _PlayerScreenState extends State<PlayerScreen> {
+  AudioPlayer player = AudioPlayer();
+
+  @override
+  void initState() {
+    player.setFilePath(widget.songInfo.data);
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +38,7 @@ class PlayerScreen extends StatelessWidget {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  SizedBox(height: 18),
+                  const SizedBox(height: 18),
                   // Music Cover Album
                   ClipRRect(
                     borderRadius: BorderRadius.circular(8),
@@ -31,7 +46,7 @@ class PlayerScreen extends StatelessWidget {
                       // controller: _audioQuery,
                       artworkHeight: 300,
                       artworkWidth: size.width * 0.80,
-                      id: songInfo.id,
+                      id: widget.songInfo.id,
                       type: ArtworkType.AUDIO,
                       artworkBorder: BorderRadius.circular(12),
                       quality: 100,
@@ -43,8 +58,8 @@ class PlayerScreen extends StatelessWidget {
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(songInfo.title, style: TextStyle(fontSize: 18, color: Colors.grey)),
-                      Text(songInfo.artist.toString(), style: TextStyle(color: Colors.grey)),
+                      Text(widget.songInfo.title, style: const TextStyle(fontSize: 18, color: Colors.grey)),
+                      Text(widget.songInfo.artist.toString(), style: const TextStyle(color: Colors.grey)),
                     ],
                   ),
                   // Playback Timeline
@@ -52,61 +67,73 @@ class PlayerScreen extends StatelessWidget {
               ),
               Column(
                 children: [
-                  Container(
-                    child: Column(
-                      children: [
-                        SliderTheme(
-                          data: SliderThemeData(
-                            overlayShape: SliderComponentShape.noOverlay,
-                            thumbShape: const RoundSliderThumbShape(enabledThumbRadius: 6),
-                            thumbColor: Colors.grey,
-                            activeTrackColor: Colors.grey.shade600,
-                            inactiveTrackColor: Colors.grey.shade800,
-                            trackHeight: 2,
-                          ),
-                          child: Slider(
-                            value: 0.6,
-                            onChanged: (value) {
-                              print(value);
-                            },
-                          ),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 6),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text('5:00', style: TextStyle(color: Colors.grey)),
-                              Text('0:00', style: TextStyle(color: Colors.grey)),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  // Playback Buttons
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  // Playback Timeline
+                  Column(
                     children: [
-                      ShuffleButton(
-                        isShuffleOn: false,
-                        onChange: (newValue) {},
+                      SliderTheme(
+                        data: SliderThemeData(
+                          overlayShape: SliderComponentShape.noOverlay,
+                          thumbShape: const RoundSliderThumbShape(enabledThumbRadius: 6),
+                          thumbColor: Colors.grey,
+                          activeTrackColor: Colors.grey.shade600,
+                          inactiveTrackColor: Colors.grey.shade800,
+                          trackHeight: 2,
+                        ),
+                        child: Slider(
+                          value: 0.6,
+                          onChanged: (value) {
+                            print(value);
+                          },
+                        ),
                       ),
-                      IconButton(onPressed: () {}, icon: const Icon(Icons.skip_previous_rounded, size: 34)),
-                      PlayButton(
-                        isPlaying: true,
-                        onChange: (bool newValue) {
-                          print(newValue);
-                        },
-                      ),
-                      IconButton(onPressed: () {}, icon: const Icon(Icons.skip_next_rounded, size: 34)),
-                      RepeatButton(
-                        isRepeatOn: false,
-                        onChange: (newValue) {},
-                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 6),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('5:00', style: TextStyle(color: Colors.grey)),
+                            Text('0:00', style: TextStyle(color: Colors.grey)),
+                          ],
+                        ),
+                      )
                     ],
                   ),
-                  SizedBox(height: 50)
+                  // Playback Controls
+                  StreamBuilder<PlayerState>(
+                    stream: player.playerStateStream,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            ShuffleButton(
+                              isShuffleOn: false,
+                              onChange: (newValue) {},
+                            ),
+                            IconButton(onPressed: () {}, icon: const Icon(Icons.skip_previous_rounded, size: 34)),
+                            PlayButton(
+                              isPlaying: snapshot.data!.playing,
+                              onTap: () {
+                                if (snapshot.data!.playing) {
+                                  player.pause();
+                                  return;
+                                }
+                                player.play();
+                              },
+                            ),
+                            IconButton(onPressed: () {}, icon: const Icon(Icons.skip_next_rounded, size: 34)),
+                            RepeatButton(
+                              isRepeatOn: false,
+                              onChange: (newValue) {},
+                            ),
+                          ],
+                        );
+                      }
+
+                      return const Text("Error");
+                    },
+                  ),
+                  const SizedBox(height: 50)
                 ],
               ),
             ],
