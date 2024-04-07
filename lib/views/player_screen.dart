b8d7eb67c2ebdfab7b3c2fdcd8ed27707c3cd7bf
sumit-coder <sumitbu24@gmail.com>
2024-04-07 22:46:00 +0530

    song ui fixes on player screen

--- lib/views/player_screen.dart
+++ lib/views/player_screen.dart
@@ -14,96 +14,104 @@ class PlayerScreen extends StatelessWidget {
     var size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.grey.shade900,
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Music Cover Album
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            // child: Image.network(
-            //   "https://i.ytimg.com/vi/725WlG1idPc/maxresdefault.jpg",
-            //   width: size.width * 0.80,
-            //   height: 300,
-            //   fit: BoxFit.cover,
-            // ),
-            child: QueryArtworkWidget(
-              // controller: _audioQuery,
-              artworkHeight: 300,
-              artworkWidth: size.width * 0.80,
-              id: songInfo.id,
-              type: ArtworkType.AUDIO,
-              artworkBorder: BorderRadius.circular(12),
-              quality: 100,
-              artworkQuality: FilterQuality.high,
-              artworkFit: BoxFit.contain,
-            ),
-          ),
-          // Track Details
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(songInfo.title, style: TextStyle(fontSize: 18, color: Colors.grey)),
-              Text(songInfo.artist.toString(), style: TextStyle(color: Colors.grey)),
-            ],
-          ),
-          // Playback Timeline
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 14),
-            child: Column(
-              children: [
-                SliderTheme(
-                  data: SliderThemeData(
-                    overlayShape: SliderComponentShape.noOverlay,
-                    thumbShape: const RoundSliderThumbShape(enabledThumbRadius: 6),
-                    thumbColor: Colors.grey,
-                    activeTrackColor: Colors.grey.shade600,
-                    inactiveTrackColor: Colors.grey.shade800,
-                    trackHeight: 2,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: 18),
+                  // Music Cover Album
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: QueryArtworkWidget(
+                      // controller: _audioQuery,
+                      artworkHeight: 300,
+                      artworkWidth: size.width * 0.80,
+                      id: songInfo.id,
+                      type: ArtworkType.AUDIO,
+                      artworkBorder: BorderRadius.circular(12),
+                      quality: 100,
+                      artworkQuality: FilterQuality.high,
+                      artworkFit: BoxFit.contain,
+                    ),
                   ),
-                  child: Slider(
-                    value: 0.6,
-                    onChanged: (value) {
-                      print(value);
-                    },
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  // Track Details
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text('5:00', style: TextStyle(color: Colors.grey)),
-                      Text('0:00', style: TextStyle(color: Colors.grey)),
+                      Text(songInfo.title, style: TextStyle(fontSize: 18, color: Colors.grey)),
+                      Text(songInfo.artist.toString(), style: TextStyle(color: Colors.grey)),
                     ],
                   ),
-                )
-              ],
-            ),
-          ),
-          // Playback Buttons
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ShuffleButton(
-                isShuffleOn: false,
-                onChange: (newValue) {},
+                  // Playback Timeline
+                ],
               ),
-              IconButton(onPressed: () {}, icon: const Icon(Icons.skip_previous_rounded, size: 34)),
-              PlayButton(
-                isPlaying: true,
-                onChange: (bool newValue) {
-                  print(newValue);
-                },
-              ),
-              IconButton(onPressed: () {}, icon: const Icon(Icons.skip_next_rounded, size: 34)),
-              RepeatButton(
-                isRepeatOn: false,
-                onChange: (newValue) {},
+              Column(
+                children: [
+                  Container(
+                    child: Column(
+                      children: [
+                        SliderTheme(
+                          data: SliderThemeData(
+                            overlayShape: SliderComponentShape.noOverlay,
+                            thumbShape: const RoundSliderThumbShape(enabledThumbRadius: 6),
+                            thumbColor: Colors.grey,
+                            activeTrackColor: Colors.grey.shade600,
+                            inactiveTrackColor: Colors.grey.shade800,
+                            trackHeight: 2,
+                          ),
+                          child: Slider(
+                            value: 0.6,
+                            onChanged: (value) {
+                              print(value);
+                            },
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 6),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('5:00', style: TextStyle(color: Colors.grey)),
+                              Text('0:00', style: TextStyle(color: Colors.grey)),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  // Playback Buttons
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ShuffleButton(
+                        isShuffleOn: false,
+                        onChange: (newValue) {},
+                      ),
+                      IconButton(onPressed: () {}, icon: const Icon(Icons.skip_previous_rounded, size: 34)),
+                      PlayButton(
+                        isPlaying: true,
+                        onChange: (bool newValue) {
+                          print(newValue);
+                        },
+                      ),
+                      IconButton(onPressed: () {}, icon: const Icon(Icons.skip_next_rounded, size: 34)),
+                      RepeatButton(
+                        isRepeatOn: false,
+                        onChange: (newValue) {},
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 50)
+                ],
               ),
             ],
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
